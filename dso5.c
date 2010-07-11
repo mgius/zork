ed7c3ed94bf9d150558350075e60bca2d96f1ec0
Mark <mgius7096@gmail.com>
2010-07-11 13:41:54 -0700

    Re-indent and quick pass to reduce compile size of dso objects

--- dso5.c
+++ dso5.c
@@ -7,182 +7,163 @@
 
 /* GTTIME-- GET TOTAL TIME PLAYED */
 
-void gttime_(t)
-integer *t;
+void gttime_(integer *t)
 {
-    integer h, m, s;
+   integer h, m, s;
 
-    itime_(&h, &m, &s);
-    *t = h * 60 + m - (time_1.shour * 60 + time_1.smin);
-    if (*t < 0) {
-	*t += 1440;
-    }
-    *t += time_1.pltime;
-    return;
+   itime_(&h, &m, &s);
+   *t = h * 60 + m - (time_1.shour * 60 + time_1.smin);
+   if (*t < 0) {
+      *t += 1440;
+   }
+   *t += time_1.pltime;
 } /* gttime_ */
 
 /* OPNCLS-- PROCESS OPEN/CLOSE FOR DOORS */
 
 /* DECLARATIONS */
 
-logical opncls_(obj, so, sc)
-integer obj;
-integer so;
-integer sc;
+logical opncls_(integer obj, integer so, integer sc)
 {
-    /* System generated locals */
-    integer i__1;
-    logical ret_val;
+   /* System generated locals */
+   logical ret_val;
 
-    ret_val = TRUE_;
-/* 						!ASSUME WINS. */
-    if (prsvec_1.prsa == vindex_1.closew) {
-	goto L100;
-    }
-/* 						!CLOSE? */
-    if (prsvec_1.prsa == vindex_1.openw) {
-	goto L50;
-    }
-/* 						!OPEN? */
-    ret_val = FALSE_;
-/* 						!LOSE */
-    return ret_val;
+   ret_val = TRUE_;
+   /* 						!ASSUME WINS. */
+   if (prsvec_1.prsa == vindex_1.closew) {
+      goto L100;
+   }
+   /* 						!CLOSE? */
+   if (prsvec_1.prsa == vindex_1.openw) {
+      goto L50;
+   }
+   /* 						!OPEN? */
+   ret_val = FALSE_;
+   /* 						!LOSE */
+   return ret_val;
 
 L50:
-    if ((objcts_1.oflag2[obj - 1] & OPENBT) != 0) {
-	goto L200;
-    }
-/* 						!OPEN... IS IT? */
-    rspeak_(so);
-    objcts_1.oflag2[obj - 1] |= OPENBT;
-    return ret_val;
+   if ((objcts_1.oflag2[obj - 1] & OPENBT) != 0) {
+      goto L200;
+   }
+   /* 						!OPEN... IS IT? */
+   rspeak_(so);
+   objcts_1.oflag2[obj - 1] |= OPENBT;
+   return ret_val;
 
 L100:
-    if (! ((objcts_1.oflag2[obj - 1] & OPENBT) != 0)) {
-	goto L200;
-    }
-/* 						!CLOSE... IS IT? */
-    rspeak_(sc);
-    objcts_1.oflag2[obj - 1] &= ~ OPENBT;
-    return ret_val;
+   if (! ((objcts_1.oflag2[obj - 1] & OPENBT) != 0)) {
+      goto L200;
+   }
+   /* 						!CLOSE... IS IT? */
+   rspeak_(sc);
+   objcts_1.oflag2[obj - 1] &= ~ OPENBT;
+   return ret_val;
 
 L200:
-    i__1 = rnd_(3) + 125;
-    rspeak_(i__1);
-/* 						!DUMMY. */
-    return ret_val;
+   rspeak_(rnd_(3) + 125);
+   /* 						!DUMMY. */
+   return ret_val;
 } /* opncls_ */
 
 /* LIT-- IS ROOM LIT? */
 
 /* DECLARATIONS */
 
-logical lit_(rm)
-integer rm;
+logical lit_(integer rm)
 {
-    /* System generated locals */
-    integer i__1, i__2;
-    logical ret_val;
+   /* System generated locals */
+   logical ret_val;
 
-    /* Local variables */
-    integer i, j, oa;
+   /* Local variables */
+   integer i, j, oa;
 
-    ret_val = TRUE_;
-/* 						!ASSUME WINS */
-    if ((rooms_1.rflag[rm - 1] & RLIGHT) != 0) {
-	return ret_val;
-    }
+   ret_val = TRUE_;
+   /* 						!ASSUME WINS */
+   if ((rooms_1.rflag[rm - 1] & RLIGHT) != 0) {
+      return ret_val;
+   }
 
-    i__1 = objcts_1.olnt;
-    for (i = 1; i <= i__1; ++i) {
-/* 						!LOOK FOR LIT OBJ */
-	if (qhere_(i, rm)) {
-	    goto L100;
-	}
-/* 						!IN ROOM? */
-	oa = objcts_1.oadv[i - 1];
-/* 						!NO */
-	if (oa <= 0) {
-	    goto L1000;
-	}
-/* 						!ON ADV? */
-	if (advs_1.aroom[oa - 1] != rm) {
-	    goto L1000;
-	}
-/* 						!ADV IN ROOM? */
+   for (i = 1; i <= objcts_1.olnt; ++i) {
+      /* 						!LOOK FOR LIT OBJ */
+      if (qhere_(i, rm)) {
+         goto L100;
+      }
+      /* 						!IN ROOM? */
+      oa = objcts_1.oadv[i - 1];
+      /* 						!NO */
+      if (oa <= 0) {
+         goto L1000;
+      }
+      /* 						!ON ADV? */
+      if (advs_1.aroom[oa - 1] != rm) {
+         goto L1000;
+      }
+      /* 						!ADV IN ROOM? */
 
-/* OBJ IN ROOM OR ON ADV IN ROOM */
+      /* OBJ IN ROOM OR ON ADV IN ROOM */
 
 L100:
-	if ((objcts_1.oflag1[i - 1] & ONBT) != 0) {
-	    return ret_val;
-	}
-	if ((objcts_1.oflag1[i - 1] & VISIBT) == 0 || (
-		objcts_1.oflag1[i - 1] & TRANBT) == 0 && (
-		objcts_1.oflag2[i - 1] & OPENBT) == 0) {
-	    goto L1000;
-	}
+      if ((objcts_1.oflag1[i - 1] & ONBT) != 0) {
+         return ret_val;
+      }
+      if ((objcts_1.oflag1[i - 1] & VISIBT) == 0 || (
+               objcts_1.oflag1[i - 1] & TRANBT) == 0 && (
+                  objcts_1.oflag2[i - 1] & OPENBT) == 0) {
+         goto L1000;
+      }
 
-/* OBJ IS VISIBLE AND OPEN OR TRANSPARENT */
+      /* OBJ IS VISIBLE AND OPEN OR TRANSPARENT */
 
-	i__2 = objcts_1.olnt;
-	for (j = 1; j <= i__2; ++j) {
-	    if (objcts_1.ocan[j - 1] == i && (objcts_1.oflag1[j - 1] & 
-		    ONBT) != 0) {
-		return ret_val;
-	    }
-/* L500: */
-	}
+      for (j = 1; j <= objcts_1.olnt; ++j) {
+         if (objcts_1.ocan[j - 1] == i && (objcts_1.oflag1[j - 1] & 
+                  ONBT) != 0) {
+            return ret_val;
+         }
+         /* L500: */
+      }
 L1000:
-	;
-    }
-    ret_val = FALSE_;
-    return ret_val;
+      ;
+   }
+   ret_val = FALSE_;
+   return ret_val;
 } /* lit_ */
 
 /* WEIGHT- RETURNS SUM OF WEIGHT OF QUALIFYING OBJECTS */
 
 /* DECLARATIONS */
 
-integer weight_(rm, cn, ad)
-integer rm;
-integer cn;
-integer ad;
+integer weight_(integer rm, integer cn, integer ad)
 {
-    /* System generated locals */
-    integer ret_val, i__1;
+   /* System generated locals */
+   integer ret_val, i__1;
 
-    /* Local variables */
-    integer i, j;
+   /* Local variables */
+   integer i, j;
 
-    ret_val = 0;
-    i__1 = objcts_1.olnt;
-    for (i = 1; i <= i__1; ++i) {
-/* 						!OMIT BIG FIXED ITEMS. */
-	if (objcts_1.osize[i - 1] >= 10000) {
-	    goto L100;
-	}
-/* 						!IF FIXED, FORGET IT. */
-	if (qhere_(i, rm) && rm != 0 || objcts_1.oadv[i - 1] == ad && ad 
-		!= 0) {
-	    goto L50;
-	}
-	j = i;
-/* 						!SEE IF CONTAINED. */
-L25:
-	j = objcts_1.ocan[j - 1];
-/* 						!GET NEXT LEVEL UP. */
-	if (j == 0) {
-	    goto L100;
-	}
-/* 						!END OF LIST? */
-	if (j != cn) {
-	    goto L25;
-	}
+   ret_val = 0;
+   for (i = 1; i <= objcts_1.olnt; ++i) {
+      /* 						!OMIT BIG FIXED ITEMS. */
+      if (objcts_1.osize[i - 1] >= 10000) {
+         continue;
+      }
+      /* 						!IF FIXED, FORGET IT. */
+      if (qhere_(i, rm) && rm != 0 || objcts_1.oadv[i - 1] == ad && ad 
+            != 0) {
+         goto L50;
+      }
+      j = i;
+      /* 						!SEE IF CONTAINED. */
+      do {
+         j = objcts_1.ocan[j - 1];
+         /* 						!GET NEXT LEVEL UP. */
+         if (j == 0) {
+            continue;
+         }
+         /* 						!END OF LIST? */
+      } while (j != cn);
 L50:
-	ret_val += objcts_1.osize[i - 1];
-L100:
-	;
-    }
-    return ret_val;
+      ret_val += objcts_1.osize[i - 1];
+   }
+   return ret_val;
 } /* weight_ */
--- dso6.c
+++ dso6.c
@@ -7,122 +7,120 @@
 #include "funcs.h"
 #include "vars.h"
 
-logical ghere_(obj, rm)
-integer obj;
-integer rm;
+logical ghere_(integer obj, integer rm)
 {
-    /* System generated locals */
-    logical ret_val;
+   /* System generated locals */
+   logical ret_val;
 
-    ret_val = TRUE_;
-/* 						!ASSUME WINS. */
-    switch (obj - star_1.strbit) {
-	case 1:  goto L1000;
-	case 2:  goto L1000;
-	case 3:  goto L1000;
-	case 4:  goto L1000;
-	case 5:  goto L1000;
-	case 6:  goto L1000;
-	case 7:  goto L1000;
-	case 8:  goto L1000;
-	case 9:  goto L1000;
-	case 10:  goto L1000;
-	case 11:  goto L1000;
-	case 12:  goto L2000;
-	case 13:  goto L3000;
-	case 14:  goto L4000;
-	case 15:  goto L5000;
-	case 16:  goto L5000;
-	case 17:  goto L5000;
-	case 18:  goto L6000;
-	case 19:  goto L7000;
-	case 20:  goto L8000;
-	case 21:  goto L9000;
-	case 22:  goto L9100;
-	case 23:  goto L8000;
-	case 24:  goto L10000;
-	case 25:  goto L11000;
-    }
-    bug_(60, obj);
+   ret_val = TRUE_;
+   /* 						!ASSUME WINS. */
+   switch (obj - star_1.strbit) {
+   case 1: 
+   case 2:
+   case 3:
+   case 4:
+   case 5:
+   case 6:
+   case 7:
+   case 8:
+   case 9:
+   case 10: 
+   case 11:  
+      return TRUE_;
+   /* 1000--	STARS ARE ALWAYS HERE */
+   case 12:  goto L2000;
+   case 13:  goto L3000;
+   case 14:  goto L4000;
+   case 15:  goto L5000;
+   case 16:  goto L5000;
+   case 17:  goto L5000;
+   case 18:  goto L6000;
+   case 19:  goto L7000;
+   case 20:  goto L8000;
+   case 21:  goto L9000;
+   case 22:  goto L9100;
+   case 23:  goto L8000;
+   case 24:  goto L10000;
+   case 25:  goto L11000;
+   }
+#ifdef DEBUG
+   bug_(60, obj);
+#endif
 
-/* 1000--	STARS ARE ALWAYS HERE */
 
-L1000:
-    return ret_val;
-
-/* 2000--	BIRD */
+   /* 2000--	BIRD */
 
 L2000:
-    ret_val = rm >= rindex_1.fore1 && rm < rindex_1.clear || rm == 
-	    rindex_1.mtree;
-    return ret_val;
+   ret_val = rm >= rindex_1.fore1 && rm < rindex_1.clear || rm == 
+      rindex_1.mtree;
+   return ret_val;
 
-/* 3000--	TREE */
+   /* 3000--	TREE */
 
 L3000:
-    ret_val = rm >= rindex_1.fore1 && rm < rindex_1.clear && rm != 
-	    rindex_1.fore3;
-    return ret_val;
+   ret_val = rm >= rindex_1.fore1 && rm < rindex_1.clear && rm != 
+      rindex_1.fore3;
+   return ret_val;
 
-/* 4000--	NORTH WALL */
+   /* 4000--	NORTH WALL */
 
 L4000:
-    ret_val = rm >= rindex_1.bkvw && rm <= rindex_1.bkbox || rm == 
-	    rindex_1.cpuzz;
-    return ret_val;
+   ret_val = rm >= rindex_1.bkvw && rm <= rindex_1.bkbox || rm == 
+      rindex_1.cpuzz;
+   return ret_val;
 
-/* 5000--	EAST, SOUTH, WEST WALLS */
+   /* 5000--	EAST, SOUTH, WEST WALLS */
 
 L5000:
-    ret_val = rm >= rindex_1.bkvw && rm < rindex_1.bkbox || rm == 
-	    rindex_1.cpuzz;
-    return ret_val;
+   ret_val = rm >= rindex_1.bkvw && rm < rindex_1.bkbox || rm == 
+      rindex_1.cpuzz;
+   return ret_val;
 
-/* 6000--	GLOBAL WATER */
+   /* 6000--	GLOBAL WATER */
 
 L6000:
-    ret_val = (rooms_1.rflag[rm - 1] & RWATER + RFILL) != 0;
-    return ret_val;
+   ret_val = (rooms_1.rflag[rm - 1] & RWATER + RFILL) != 0;
+   return ret_val;
 
-/* 7000--	GLOBAL GUARDIANS */
+   /* 7000--	GLOBAL GUARDIANS */
 
 L7000:
-    ret_val = rm >= rindex_1.mrc && rm <= rindex_1.mrd || rm >= 
-	    rindex_1.mrce && rm <= rindex_1.mrdw || rm == rindex_1.inmir;
-    return ret_val;
+   ret_val = rm >= rindex_1.mrc && rm <= rindex_1.mrd || rm >= 
+      rindex_1.mrce && rm <= rindex_1.mrdw || rm == rindex_1.inmir;
+   return ret_val;
 
-/* 8000--	ROSE/CHANNEL */
+   /* 8000--	ROSE/CHANNEL */
 
 L8000:
-    ret_val = rm >= rindex_1.mra && rm <= rindex_1.mrd || rm == 
-	    rindex_1.inmir;
-    return ret_val;
+   ret_val = rm >= rindex_1.mra && rm <= rindex_1.mrd || rm == 
+      rindex_1.inmir;
+   return ret_val;
 
-/* 9000--	MIRROR */
-/* 9100		PANEL */
+   /* 9000--	MIRROR */
+   /* 9100		PANEL */
 
 L9100:
-    if (rm == rindex_1.fdoor) {
-	return ret_val;
-    }
-/* 						!PANEL AT FDOOR. */
+   if (rm == rindex_1.fdoor) {
+      return ret_val;
+   }
+   /* 						!PANEL AT FDOOR. */
 L9000:
-    ret_val = rm >= rindex_1.mra && rm <= rindex_1.mrc || rm >= 
-	    rindex_1.mrae && rm <= rindex_1.mrcw;
-    return ret_val;
+   ret_val = rm >= rindex_1.mra && rm <= rindex_1.mrc || rm >= 
+      rindex_1.mrae && rm <= rindex_1.mrcw;
+   return ret_val;
 
-/* 10000--	MASTER */
+   /* 10000--	MASTER */
 
 L10000:
-    ret_val = rm == rindex_1.fdoor || rm == rindex_1.ncorr || rm == 
-	    rindex_1.parap || rm == rindex_1.cell;
-    return ret_val;
+   ret_val = rm == rindex_1.fdoor || rm == rindex_1.ncorr || rm == 
+      rindex_1.parap || rm == rindex_1.cell;
+   return ret_val;
 
-/* 11000--	LADDER */
+   /* 11000--	LADDER */
 
 L11000:
-    ret_val = rm == rindex_1.cpuzz;
-    return ret_val;
+   ret_val = rm == rindex_1.cpuzz;
+   return ret_val;
 
 } /* ghere_ */
 
@@ -130,41 +128,40 @@ L11000:
 
 /* DECLARATIONS */
 
-integer mrhere_(rm)
-integer rm;
+integer mrhere_(integer rm)
 {
-    /* System generated locals */
-    integer ret_val, i__1;
+   /* System generated locals */
+   integer ret_val;
 
-    if (rm < rindex_1.mrae || rm > rindex_1.mrdw) {
-	goto L100;
-    }
+   if (rm < rindex_1.mrae || rm > rindex_1.mrdw) {
+      goto L100;
+   }
 
-/* RM IS AN E-W ROOM, MIRROR MUST BE N-S (MDIR= 0 OR 180) */
+   /* RM IS AN E-W ROOM, MIRROR MUST BE N-S (MDIR= 0 OR 180) */
 
-    ret_val = 1;
-/* 						!ASSUME MIRROR 1 HERE. */
-    if ((rm - rindex_1.mrae) % 2 == findex_1.mdir / 180) {
-	ret_val = 2;
-    }
-    return ret_val;
+   ret_val = 1;
+   /* 						!ASSUME MIRROR 1 HERE. */
+   if ((rm - rindex_1.mrae) % 2 == findex_1.mdir / 180) {
+      ret_val = 2;
+   }
+   return ret_val;
 
-/* RM IS NORTH OR SOUTH OF MIRROR.  IF MIRROR IS N-S OR NOT */
-/* WITHIN ONE ROOM OF RM, LOSE. */
+   /* RM IS NORTH OR SOUTH OF MIRROR.  IF MIRROR IS N-S OR NOT */
+   /* WITHIN ONE ROOM OF RM, LOSE. */
 
 L100:
-    ret_val = 0;
-    if ((i__1 = findex_1.mloc - rm, abs(i__1)) != 1 || findex_1.mdir % 180 ==
-	     0) {
-	return ret_val;
-    }
+   ret_val = 0;
+   if ((abs(findex_1.mloc - rm)) != 1 || findex_1.mdir % 180 ==
+         0) {
+      return ret_val;
+   }
 
-/* RM IS WITHIN ONE OF MLOC, AND MDIR IS E-W */
+   /* RM IS WITHIN ONE OF MLOC, AND MDIR IS E-W */
 
-    ret_val = 1;
-    if (rm < findex_1.mloc && findex_1.mdir < 180 || rm > findex_1.mloc && 
-	    findex_1.mdir > 180) {
-	ret_val = 2;
-    }
-    return ret_val;
+   ret_val = 1;
+   if (rm < findex_1.mloc && findex_1.mdir < 180 || rm > findex_1.mloc && 
+         findex_1.mdir > 180) {
+      ret_val = 2;
+   }
+   return ret_val;
 } /* mrhere_ */
